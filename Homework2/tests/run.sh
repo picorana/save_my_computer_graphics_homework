bf01ee31164b7d120ea37b2c3362854902a1bf7f
picorana <dibartolomeo.sara@gmail.com>
2016-11-04 17:45:07 +0100

    elegant solution is elegant

---
+++ Homework2/tests/run.sh
@@ -0,0 +1,16 @@
+../bin/model 01_simple.json
+../bin/model 02_geom.json
+../bin/model 03_plane.json
+../bin/model 04_woman.json
+../bin/model 05_cartoon_girl.json
+
+../bin/model 11_spline.json
+../bin/model 12_hair.json
+../bin/model 13_subdivcube.json
+../bin/model 14_subdivmonkey.json
+
+../bin/model 21_textured.json
+../bin/model 22_bumped.json
+../bin/model 23_shuttle.json
+../bin/model 24_head.json
+../bin/model 25_sibenik.json
---
+++ Homework2/tests/test.bat
@@ -0,0 +1,3 @@
+copy ..\src\model_fragment.glsl
+copy ..\src\model_vertex.glsl
+..\bin\Release\model.exe 12_hair.json
